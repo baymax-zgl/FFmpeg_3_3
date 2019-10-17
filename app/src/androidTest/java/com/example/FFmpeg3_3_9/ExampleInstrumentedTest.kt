c279d8555087a0c9618a2bf8433673356ebdee14
张国梁 <zhangguoliang@hudunsoft.com>
2019-10-17 15:47:32 +0800

    删除无用代码

--- app/src/androidTest/java/com/example/FFmpeg3_3_9/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/FFmpeg3_3_9/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.example.FFmpeg3_3_9
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
